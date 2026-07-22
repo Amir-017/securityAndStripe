2fff519098c12a639e32c76569c4192c43950bf9
amir whdan <amiirahmed017@gmail.com>
2026-07-22 22:52:14 +0300

    solve issue that showen when deploy project in page auth service

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,8 +1,7 @@
 import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
-import { Role, User, UserDocument } from 'src/users/schemas/userSchema';
+
+import { Role, User, UserDocument } from '../users/schemas/userSchema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
